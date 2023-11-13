ba08ed9a6821d15cc66b629e012c88a6adff7182
mha13 <111089847+mha13@users.noreply.github.com>
2023-11-13 13:13:50 +0100

    new modification for the react porject

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
-function Heading() {
-  let title = 'heading';
-  return (
-    <h1>{title}</h1>
+function Heading(props) {
+  // return (
+  //   <h1>{props.title}</h1>
+  // );
+  // return React.createElement(
+  //   "h1",
+  //   { className: 'greeting',name: 'Taylor' },
+  //   props.title,
+  //   createElement('i', null, props.title2),
+  //   '. Welcome!'
+  // );
+
+  var owner = props.owner+' ';
+  var title= props.title+' ';
+  return createElement(
+    'h1',
+    { className: 'hello', id: 'hello' },
+    title,
+    createElement('i', null, owner),
+    '!'
   );
+}
+
+
+function Greeting({ name }) {
+  // return (
+  //   <h1 className="greeting">
+  //     <i>{name}</i>. Welcome!
+  //   </h1>
+  // );
+  return createElement(
+    'h1',
+    {className:'test'},
+    'welcome ' ,createElement('i',null,name), ' man'
+  )
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Heading />
+        <Heading title='Hello' owner='Hossein'  />
+        <Greeting name="Hossein" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Fuck <code>src/App.js</code> and save to reload.
